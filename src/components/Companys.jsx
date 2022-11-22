9fdd89120b852ed2d9f44b972c90f1bbb72e7fc4
Cristian Calderon <cdcalderonrojas@gmail.com>
2022-11-22 14:12:11 +0100

    Agreacion de carpetas - Rutas creadas

---
+++ src/components/Companys.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Companys = () => {
+  return (
+    <div className='container-companys'>
+        <img src="" alt="" />
+        <div className='companys-stars'>
+            
+        </div>
+    </div>
+  )
+}

73fa7b114e9023716cb13c4e7b5d62733ff83079
Erick crespin <106453089+ecp12@users.noreply.github.com>
2022-11-25 09:02:29 +0100

    Revert "Create app form + responsive"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import '../src/styles/App.css';
+import './App.css';
 import NavBar from '../src/components/nav/Nav.jsx';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
   return (
     <div>

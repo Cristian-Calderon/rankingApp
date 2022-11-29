fa3519be11c61ef75f9873990a662c2b7ba4c873
Cristian Calderon <cdcalderonrojas@gmail.com>
2022-11-29 11:21:01 +0100

    Top 10 realizado

--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -11,25 +11,26 @@ export const Rank = () => {
       <div className="rank">
       {
       Empresas.map((variable) => {
-        return (
-          <div className="rank-padding" key={variable.id}>
-            <div className="rank-img">
-              <div className="img-log">
-              <img src={variable.Column3} alt="logo" />
+        if(variable.Column2.length > 8){
+          return (
+            <div className="rank-padding" key={variable.id}>
+              <div className="rank-img">
+                <div className="img-log">
+                <img src={variable.Column3} alt="logo" />
+                </div>
+                <div className="img-name">
+                <p>{variable.Column1}</p>
+                </div>
               </div>
-              <div className="img-name">
-              <p>{variable.Column1}</p>
+              <div className="description">
+                <p><span>Description: </span>{variable.Column4}</p>
+              </div>
+              <div className="stars">
+                <p>{variable.Column2}</p>
               </div>
             </div>
-            <div className="description">
-              <p><span>Description: </span>{variable.Column4}</p>
-            </div>
-            <div className="stars">
-              <p>{variable.Column2}</p>
-              <p>{variable.Column2.length} Stars</p>
-            </div>
-          </div>
-        );
+          );
+        }
       })}
     </div>
   </div>

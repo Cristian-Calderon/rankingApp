41603423fde3b8062319056bf2a162d7b2a647ce
Francisco Aguirr <fran.uru.aguirre@gmail.com>
2022-12-01 10:21:31 +0100

    actualización a rama develop

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './components/style/index.css';
+import '../src/components/style/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
